fd06126d2dbbdac63044d719cd2b47b28a55ad81
wookieefuzz <wookie.fuzz@gmail.com>
2015-01-17 22:56:04 -0500

    added prints to debug

--- constraintAnalysis/constraintCalculations.py
+++ constraintAnalysis/constraintCalculations.py
@@ -4,7 +4,9 @@ class constraintCalculations:
         self.d = design
         
     def handLaunchConstraint(self):
-        
+        print self.d.rho
+        print self.d.ClMax
+        print self.d.vHL
         wl = .5 * self.d.rho * self.d.ClMax * (self.d.vHL/1.2)**2
         
         return wl
