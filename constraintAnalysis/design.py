da461ab86f90c3da555afbca52a2e7b0c9343499
wookieefuzz <wookie.fuzz@gmail.com>
2015-01-17 22:59:14 -0500

    cast data from google drive to floats

--- constraintAnalysis/design.py
+++ constraintAnalysis/design.py
@@ -1,19 +1,19 @@
 class design:
     
     def __init__(self,AR,e,rho,etaP,etaM,LoDMax,RofC,vCruise,cd0,N,vHL,vMax,ClMax):
-        self.AR = AR
-        self.e = e
-        self.rho = rho
-        self.etaP = etaP
-        self.etaM = etaM
-        self.LoDMax = LoDMax
-        self.RofC = RofC
-        self.vCruise = vCruise
-        self.cd0 = cd0
-        self.N = N
-        self.vHL = vHL
-        self.vMax = vMax
-        self.ClMax = ClMax
+        self.AR = float(AR)
+        self.e = float(e)
+        self.rho = float(rho)
+        self.etaP = float(etaP)
+        self.etaM = float(etaM)
+        self.LoDMax = float(LoDMax)
+        self.RofC = float(RofC)
+        self.vCruise = float(vCruise)
+        self.cd0 = float(cd0)
+        self.N = float(N)
+        self.vHL = float(vHL)
+        self.vMax = float(vMax)
+        self.ClMax = float(ClMax)
         
         
         
