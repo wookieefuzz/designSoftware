7abd3cb116175d2f1a5265184fb425aadf4df9d4
wookieefuzz <wookie.fuzz@gmail.com>
2015-01-21 23:19:39 -0500

    added logger for ag uav

---
+++ AgUAVlogger.py
@@ -0,0 +1,21 @@
+import socket
+import re
+
+host = ''
+port = 5903
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.bind((host,port))
+sock.listen(1)
+
+while True:
+    csock,caddr = sock.accept()
+    print "connection from: " + 'caddr'
+    req = csock.recv(1024)
+    print req
+    fo = open("agUAVlog.txt","a")
+    fo.write(req)
+    fo.write("\n")
+    fo.close()
+    csock.close()
+    
+    
