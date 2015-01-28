2fb17686d2a0bd3f83a068fcef287c8d26b2494b
wookieefuzz <wookie.fuzz@gmail.com>
2015-01-28 10:25:10 -0500

    fixed matlab porting errrors

--- weightAnalysis/weightAnalysis.py
+++ weightAnalysis/weightAnalysis.py
@@ -161,24 +161,25 @@ weightSheet.update_acell('H5',W0lbs)
 
 
 def loiterCalc(vL,t,etaM,etaP,kBatt,LD):
-    weightFraction = (vL * t) / (etaM * etaP * kBatt * LD);
+    weightFraction = (vL * t) / (etaM * etaP * kBatt * LD)
     return weightFraction
 
 def cruiseCalc(range,etaM,etaP,kBatt,LDc):
-    weightFraction = range / (etaM * etaP * kBatt * LDc);
+    weightFraction = range / (etaM * etaP * kBatt * LDc)
     return weightFraction
 
 def turnCalc(WS,rho,clmax,k,PW,etaM,etaP,cd0,theta,kBatt):
-    g = 9.81;
+    g = 9.81
 
-    vT = 1.2 * math.sqrt(2.0 * WS / (rho * clmax));
+    vT = 1.2 * math.sqrt(2.0 * WS / (rho * clmax))
 
-    q = .5 * rho * vT^2.0;
+    q = .5 * rho * vT^2.0
 
-    t1 = q / (k*WS);
-    t2 = (PW * etaM * etaP) / vT;
-    t3 = q*cd0/WS;
+    t1 = q / (k*WS)
+    t2 = (PW * etaM * etaP) / vT
+    t3 = q*cd0/WS
 
-    n = math.sqrt(t1*(t2 -t3));
+    n = math.sqrt(t1*(t2 -t3))
 
-    weightFraction = (2 * math.pi * turns * PW * vT) / (kBatt * g * math.sqrt(n^2.0 - 1.0));
+    weightFraction = (2 * math.pi * turns * PW * vT) / (kBatt * g * math.sqrt(n^2.0 - 1.0))
+    return weightFraction
