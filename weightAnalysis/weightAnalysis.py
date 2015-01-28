f33742d665aba7d67fabadf74ab1ad3636015cbd
wookieefuzz <wookie.fuzz@gmail.com>
2015-01-28 10:14:58 -0500

    added casts to floats

--- weightAnalysis/weightAnalysis.py
+++ weightAnalysis/weightAnalysis.py
@@ -30,57 +30,57 @@ weightSheet = file.worksheet("WeightAnalysis")
 # now go through and find the needed data
 cell = weightSheet.find("AR")
 print cell
-AR = weightSheet.cell(cell.row,2).value
+AR = float(weightSheet.cell(cell.row,2).value)
 print AR
 
 cell = weightSheet.find("e")
 print cell
-e = weightSheet.cell(cell.row,2).value
+e = float(weightSheet.cell(cell.row,2).value)
 print e
 
 cell = weightSheet.find("rho")
 print cell
-rho = weightSheet.cell(cell.row,2).value
+rho = float(weightSheet.cell(cell.row,2).value)
 print rho   
 
 cell = weightSheet.find("etaP")
-etaP = weightSheet.cell(cell.row,2).value
+etaP = float(weightSheet.cell(cell.row,2).value)
 print etaP
 
 cell = weightSheet.find("etaM")
-etaM = weightSheet.cell(cell.row,2).value
+etaM = float(weightSheet.cell(cell.row,2).value)
 print etaM  
 
 cell = weightSheet.find("LoDMax")
-LoDMax = weightSheet.cell(cell.row,2).value
+LoDMax = float(weightSheet.cell(cell.row,2).value)
 print LoDMax
 
 cell = weightSheet.find("RofC")
-RofC = weightSheet.cell(cell.row,2).value
+RofC = float(weightSheet.cell(cell.row,2).value)
 print RofC
 
 cell = weightSheet.find("vCruise")
-vCruise = weightSheet.cell(cell.row,2).value
+vCruise = float(weightSheet.cell(cell.row,2).value)
 print vCruise
 
 cell = weightSheet.find("cd0")
-cd0 = weightSheet.cell(cell.row,2).value
+cd0 = float(weightSheet.cell(cell.row,2).value)
 print cd0
 
 cell = weightSheet.find("N")
-N = weightSheet.cell(cell.row,2).value
+N = float(weightSheet.cell(cell.row,2).value)
 print N
 
 cell = weightSheet.find("vHL")
-vHL = weightSheet.cell(cell.row,2).value
+vHL = float(weightSheet.cell(cell.row,2).value)
 print vHL
 
 cell = weightSheet.find("vMax")
-vMax = weightSheet.cell(cell.row,2).value
+vMax = float(weightSheet.cell(cell.row,2).value)
 print vMax
 
 cell = weightSheet.find("ClMax")
-ClMax = weightSheet.cell(cell.row,2).value
+ClMax = float(weightSheet.cell(cell.row,2).value)
 print ClMax
 
 #-------------------------------------------
@@ -89,39 +89,39 @@ print ClMax
 
 
 cell = weightSheet.find("turns")
-turns = weightSheet.cell(cell.row,2).value
+turns = float(weightSheet.cell(cell.row,2).value)
 print turns  
 
 cell = weightSheet.find("W/S")
-WS = weightSheet.cell(cell.row,2).value
+WS = float(weightSheet.cell(cell.row,2).value)
 print WS
 
 cell = weightSheet.find("P/W")
-PW = weightSheet.cell(cell.row,2).value
+PW = float(weightSheet.cell(cell.row,2).value)
 print PW 
 
 cell = weightSheet.find("WeW0")
-WeW0 = weightSheet.cell(cell.row,2).value
+WeW0 = float(weightSheet.cell(cell.row,2).value)
 print WeW0
 
 cell = weightSheet.find("Wpl")
-Wpl = weightSheet.cell(cell.row,2).value
+Wpl = float(weightSheet.cell(cell.row,2).value)
 print Wpl
 
 cell = weightSheet.find("kBatt")
-kBatt = weightSheet.cell(cell.row,2).value
+kBatt = float(weightSheet.cell(cell.row,2).value)
 print kBatt
 
 cell = weightSheet.find("cruiseRange")
-xBR = weightSheet.cell(cell.row,2).value
+xBR = float(weightSheet.cell(cell.row,2).value)
 print xBR
 
 cell = weightSheet.find("loiterTime")
-t = weightSheet.cell(cell.row,2).value
+t = float(weightSheet.cell(cell.row,2).value)
 print t
 
 cell = weightSheet.find("loiterSpeed")
-vL = weightSheet.cell(cell.row,2).value
+vL = float(weightSheet.cell(cell.row,2).value)
 print vL
 
 
