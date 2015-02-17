38eaeeeb8a9ca03de71d5ecca32c78edb3be8f9e
wookieefuzz <wookie.fuzz@gmail.com>
2015-02-17 19:07:27 +0000

    minor changes

--- collaborationSuite/scheduler.py
+++ collaborationSuite/scheduler.py
@@ -2,10 +2,10 @@ import time
 from scraper import scraper
 from dataVariable import dataVariable
 
-key = '1ajTeTYzpvzRVa51Jtoc9_mnrx1Nmu_9UvZ6YnRlkLjE'
+key = '1ajTeTYzpvzRVa51Jtoc9_mnrx1Nmu_9UvZ6YnRlkLjE' # key to doc
 sheetName = 'Aircraft Parameters'
-#botID = '8c74b68834ee6321b461634b8b'
-botID = '9643f965e64f584aabb71af780'
+#botID = '8c74b68834ee6321b461634b8b' # test group
+botID = '9643f965e64f584aabb71af780'  # ag uav group
 
 S = scraper(key)
 
