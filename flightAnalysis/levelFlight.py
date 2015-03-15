a61a9bb98e6410da6eb2e0228c7eb4e4e369cbc4
wookieefuzz <wookie.fuzz@gmail.com>
2015-03-15 23:48:56 +0000

    added gspread

--- flightAnalysis/levelFlight.py
+++ flightAnalysis/levelFlight.py
@@ -1,5 +1,10 @@
 from missionLeg import missionLeg
 
 class levelFlight(missionLeg):
-    def __init__(self,name):
+    
+    def __init__(self,name,distance,speed,density):
         self.name = name
+        self.distance = distance
+        self.speed = speed
+        self.density = density 
+        self.time = distance / speed
